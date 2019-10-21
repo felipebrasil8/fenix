fa90f98ffab0639e7e86df8cf3f4d68b09496689
Felipe Brasil <felipe.almeida@locaweb.com.br>
2019-10-21 19:20:16 -0300

    Adjust files

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'git...'
-#git clone git@bitbucket.org:novaxtelecomdev/fenix.git
+#git clone git@github.com:felipebrasil8/fenix.git
 #mv fenix/* .
 #rm -rf fenix
 docker-compose build && docker-compose up -d && echo "Please wait while service is up..."
